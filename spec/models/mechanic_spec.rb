033b7b81aeb912de056912fa6678ccd4ef714814
Gaby Mendez <gmendez90@gmail.com>
2020-07-17 12:17:33 -0400

    Add relationship specs to Ride and Mechanic

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,4 +1,7 @@
 RSpec.describe Mechanic do
   it { should validate_presence_of :name }
   it { should validate_presence_of :years_of_experience }
+
+  it { should have_many :ride_mechanics }
+  it { should have_many :rides, through: :ride_mechanics }
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,3 +1,6 @@
 RSpec.describe Ride do
   it { should belong_to :park }
+
+  it { should have_many :ride_mechanics }
+  it { should have_many :mechanics, through: :ride_mechanics }
 end
