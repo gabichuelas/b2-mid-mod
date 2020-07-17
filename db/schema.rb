0e559f3350ce0789fedc2360c4b4b85188196def
Gaby Mendez <gmendez90@gmail.com>
2020-07-17 11:29:15 -0400

    Create Park model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717151347) do
+ActiveRecord::Schema.define(version: 20200717152702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20200717151347) do
     t.integer "years_of_experience"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "parks", force: :cascade do |t|
+    t.string "name"
+    t.float "admission"
   end
 
 end
