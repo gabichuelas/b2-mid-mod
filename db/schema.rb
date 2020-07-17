a995f5f2e28e8740a5a7098a44341455416f7dbb
Gaby Mendez <gmendez90@gmail.com>
2020-07-17 12:22:56 -0400

    Create RideMechanics migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200717153154) do
+ActiveRecord::Schema.define(version: 20200717162227) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,14 +25,29 @@ ActiveRecord::Schema.define(version: 20200717153154) do
   create_table "parks", force: :cascade do |t|
     t.string "name"
     t.float "admission"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "ride_mechanics", force: :cascade do |t|
+    t.bigint "ride_id"
+    t.bigint "mechanic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mechanic_id"], name: "index_ride_mechanics_on_mechanic_id"
+    t.index ["ride_id"], name: "index_ride_mechanics_on_ride_id"
   end
 
   create_table "rides", force: :cascade do |t|
     t.string "name"
     t.integer "rating"
     t.bigint "park_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.index ["park_id"], name: "index_rides_on_park_id"
   end
 
+  add_foreign_key "ride_mechanics", "mechanics"
+  add_foreign_key "ride_mechanics", "rides"
   add_foreign_key "rides", "parks"
 end
