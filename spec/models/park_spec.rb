6e361795593f279e35689b1a8175cbd073e4dcad
Gaby Mendez <gmendez90@gmail.com>
2020-07-17 11:49:41 -0400

    Fix typos

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe Park do
       @hershey = Park.create!(name: 'Hershey Park', admission: 50.00)
 
       @light_racer = @hershey.rides.create!(name: 'Lightning Racer', rating: 6)
-      @storm_runner = @hershey.rides.create!(name: 'Storm Rummer', rating: 7)
+      @storm_runner = @hershey.rides.create!(name: 'Storm Runner', rating: 7)
       @bear = @hershey.rides.create!(name: 'The Great Bear', rating: 8)
     end
 
