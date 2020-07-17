bf460fe6019cd1aa11959251df433a8cc58f923a
Gaby Mendez <gmendez90@gmail.com>
2020-07-17 14:00:48 -0400

    Reorder ride creation in park_spec

--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe Park do
     before :each do
       @hershey = Park.create!(name: 'Hershey Park', admission: 50.00)
 
-      @light_racer = @hershey.rides.create!(name: 'Lightning Racer', rating: 6)
-      @storm_runner = @hershey.rides.create!(name: 'Storm Runner', rating: 7)
       @bear = @hershey.rides.create!(name: 'The Great Bear', rating: 8)
+      @storm_runner = @hershey.rides.create!(name: 'Storm Runner', rating: 7)
+      @light_racer = @hershey.rides.create!(name: 'Lightning Racer', rating: 6)
     end
 
     it '#avg_trhill_rating: can calculate average rating for all rides in a given park' do
