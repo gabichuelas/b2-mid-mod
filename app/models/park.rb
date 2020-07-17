0506b0aa15b307c2ecc06a95c042bdafaf57fdc2
Gaby Mendez <gmendez90@gmail.com>
2020-07-17 11:47:27 -0400

    Write park#avg_thrill_rating method

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,3 +1,7 @@
 class Park < ApplicationRecord
   has_many :rides
+
+  def avg_thrill_rating
+    self.rides.average(:rating)
+  end
 end
--- spec/models/park_spec.rb
+++ spec/models/park_spec.rb
@@ -1,3 +1,19 @@
 RSpec.describe Park do
   it { should have_many :rides }
+
+  describe '#avg_thrill_rating' do
+
+    before :each do
+      @hershey = Park.create!(name: 'Hershey Park', admission: 50.00)
+
+      @light_racer = @hershey.rides.create!(name: 'Lightning Racer', rating: 6)
+      @storm_runner = @hershey.rides.create!(name: 'Storm Rummer', rating: 7)
+      @bear = @hershey.rides.create!(name: 'The Great Bear', rating: 8)
+    end
+
+    it 'can calculate average rating for all rides in a given park' do
+
+      expect(@hershey.avg_thrill_rating).to eq(7)
+    end
+  end
 end
